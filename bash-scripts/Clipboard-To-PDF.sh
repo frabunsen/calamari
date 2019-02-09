947882bbd5a55f49cea7751c1f21337be2dc752a
volcanpc <frabunsen@gmail.com>
2019-02-09 09:51:39 +0100

    Added some stuff

---
+++ bash-scripts/Clipboard-To-PDF.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Need python pyperclip package (pip install pyperclip)
+# Need python fpdf package (pip install pyperclip)
+
+python <(cat <<EoF
+
+import os
+import sys
+import glob
+import pyperclip
+from fpdf import FPDF
+
+
+NAME = "/home/sensi/OUTPDF-"
+
+pdfs = glob.glob(NAME + "*.pdf")
+num = len(pdfs) + 1
+
+OUTPDF = "".join([NAME,str(num),".pdf"])
+
+pdf = FPDF()
+pdf.add_page()
+pdf.set_xy(0, 0)
+pdf.set_font("helvetica", "", 10.0)
+pdf.multi_cell(0, 5, txt=pyperclip.paste())
+pdf.output(OUTPDF, "F")
+
+EoF
+)
